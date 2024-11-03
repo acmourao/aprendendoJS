c650e4b856be342ba4f16ae2d859d44564ded32f
acmourao <amourao@stj.jus.br>
2024-11-03 03:14:43 -0300

    add service e icon

--- js/common.js
+++ js/common.js
@@ -1,6 +1,25 @@
 var app = angular.module("myApp", []);
-app.controller("myCtrl", function ($scope) {
+
+app.controller('layoutController', ['$scope', 'CtrlService', function ($scope, ctrlService) {
     $scope.firstName = "John";
     $scope.lastName = "Doe";
-    $scope.meuNome = "zé";
-});
+    $scope.meuNome = "zé do caixão";
+
+    $scope.saveLayoutToAPI = function () {
+        ctrlService.save($scope);
+    }
+    $scope.loadLayoutFromAPI = function () {
+        ctrlService.load($scope);
+    };
+
+}]);
+
+app.service('CtrlService', ['$log', function ($log) {
+    this.save = function ($scope) {
+        $log.log('Layout saved.' + $scope.meuNome);
+    };
+
+    this.load = function ($scope) {
+        $log.log('Layout loaded.' + $scope.meuNome);
+    };
+}]);
